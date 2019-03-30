5d4d03b3841bd7ebce7e764a8d695eea02e17ab9
Daniel Lauzon <daniel.lauzon@gmail.com>
2019-03-30 17:48:00 -0400

    Only One file, or type generation breks

--- lib-simple/src/index.js
+++ lib-simple/src/index.js
@@ -1,3 +1,20 @@
 
-export { Greeter } from './Greeter'
-export { Welcome } from './Welcome'
+// // These break!!!!
+// import { Welcome } from './Welcome'
+// import { Greeter } from './Greeter'
+
+// export {
+//   Greeter, Welcome
+// }
+
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export const Greeter = (name) => `Hello ${name}, from a library`
+
+export function Welcome ({ name = 'Dan-eroo' }) {
+  return (<h4>Hello, {name}</h4>)
+}
+
+Welcome.propTypes = { name: PropTypes.string }
+Welcome.defaultProps = { name: 'Dan-eroo' }
