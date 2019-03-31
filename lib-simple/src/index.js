c046aa8e8015cc62bd8e51d89970077d6ac36358
Daniel Lauzon <daniel.lauzon@gmail.com>
2019-03-30 20:44:38 -0400

    Exports and types working

--- lib-simple/src/index.js
+++ lib-simple/src/index.js
@@ -1,10 +1,3 @@
 
-// // These break!!!!
-import Welcome from './Welcome' // eslint-disable-line no-unused-vars
-import Greeter from './Greeter' // eslint-disable-line no-unused-vars
-
 export { Welcome } from './Welcome'
 export { Greeter } from './Greeter'
-
-// This didn't help @pika/plugin-build-types
-// export default { Welcome, Greeter }
